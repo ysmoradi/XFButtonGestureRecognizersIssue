d7fe273e6c1d5254ec2747464b0ddabc281852ef
Yaser Moradi <ysmoradi@outlook.com>
2018-11-08 02:03:46 +0330

    Issue repo

---
+++ src/App/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace XFButtonGestureRecognizersIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            ((VisualElement)sender).BackgroundColor = Color.LightBlue;
+        }
+    }
+}
